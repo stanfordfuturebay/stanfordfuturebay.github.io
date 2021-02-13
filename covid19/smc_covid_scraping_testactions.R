b6e215837d5eab4c84d6191a538dded9b7c8a432
sspeizer <63312222+sspeizer@users.noreply.github.com>
2021-02-13 14:19:16 -0500

    Update smc_covid_scraping_testactions.R

--- covid19/smc_covid_scraping_testactions.R
+++ covid19/smc_covid_scraping_testactions.R
@@ -13,6 +13,6 @@ remDr <- remoteDriver(
 remDr$open()
 
 # first getting testing data
-remDr$navigate("https://app.powerbigov.us/view?r=eyJrIjoiMWI5NmE5M2ItOTUwMC00NGNmLWEzY2UtOTQyODA1YjQ1NWNlIiwidCI6IjBkZmFmNjM1LWEwNGQtNDhjYy1hN2UzLTZkYTFhZjA4ODNmOSJ9")
+remDr$navigate("https://www.smchealth.org/data-dashboard/lab-testing-data")
 
 remDr$close()
